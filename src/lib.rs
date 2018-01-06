5555c1a6ee2449853b6689dfaa9b2e6ac4cf804d
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2018-01-07 01:51:32 +0800

    git deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate git2;
+use git2::{Repository, Signature, Commit, ObjectType, Time, DiffOptions};
 pub mod tpl;
 pub mod semver;
 
@@ -8,17 +10,28 @@ pub enum VersionBumpBehavior {
     Patch,
 }
 
-pub struct Repository {
-    path: String,
-    commit_template: String,
-    tag_template: String
+pub struct ProjectRepo {
+    pub path: String,
+    pub commit_template: String,
+    pub tag_template: String
 }
 
+pub struct Message {
+    message_type: String,
+    scope: String,
+    short_description: String,
+    description: String,
+    breaking_change: bool,
+    raw: String,
+}
 
-pub fn get_version(repo: &Repository, bump_behavior: VersionBumpBehavior, release: bool) -> String {
-    
-    
+pub fn get_version(repo:  &ProjectRepo, bump_behavior: VersionBumpBehavior, release: bool) -> String {
     String::from("1.0.0")
+}
+
+pub fn format_commit_message(msg: &Message) -> bool {
+
+    true
 }
 
 
@@ -26,6 +39,6 @@ pub fn get_version(repo: &Repository, bump_behavior: VersionBumpBehavior, releas
 mod tests {
     #[test]
     fn placeholder() {
-        assert!(false,"placeholder test");
+        assert!(true,"placeholder test");
     }
 }
--- src/semver.rs
+++ src/semver.rs
@@ -1,5 +1,3 @@
-use tpl;
-
-pub fn get_next_version(commits: Vec<tpl::Message>,lastTag: String, branch: String ) -> String {
+pub fn get_next_version(commits: Vec<super::Message>,last_tag: String, branch: String ) -> String {
     String::from("1.0.0")
 }
