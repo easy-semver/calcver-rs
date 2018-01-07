b165975f8aab08ff643d449569c52c26a2a4ddd4
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2018-01-07 23:10:29 +0800

    feat: commit message formatting
    commit message formatting,
    unit tests,
    some error handling

---
+++ src/error.rs
@@ -0,0 +1,63 @@
+use std::error::Error;
+use std::fmt;
+use handlebars;
+
+
+quick_error! {
+    #[derive(PartialEq, Debug, Clone)]
+    pub enum CalcverErrorReason {
+        Unknown {
+            display("I dunno what the error is!!!")
+            description("Unknown!")
+        }
+        Handlebars(c: String) {
+            display("Handlebars error: {:?}", c)
+            description("Handlebars error")
+        }
+    }
+}
+
+
+/// Generic error for now
+#[derive(Debug, PartialEq)]
+pub struct CalcverError {
+    pub reason: CalcverErrorReason,
+}
+
+impl CalcverError {
+    pub fn of(e: CalcverErrorReason) -> CalcverError {
+        CalcverError {
+            reason: e,
+        }
+    }
+    pub fn with<E>(cause: E) -> CalcverError
+        where
+        E: Error + 'static,
+    {
+        CalcverError::of(CalcverErrorReason::Handlebars(cause.description().to_string()))
+    }
+}
+
+impl Error for CalcverError {
+    fn description(&self) -> &str {
+        self.reason.description()
+    }
+}
+
+impl fmt::Display for CalcverError {
+    fn fmt(&self, e: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(e, "Error: {}",self.reason.description())
+    }
+}
+
+
+impl From<handlebars::TemplateError> for CalcverError {
+     fn from(e: handlebars::TemplateError) -> CalcverError {
+        CalcverError::with(e)
+    }
+}
+impl From<handlebars::RenderError> for CalcverError {
+     fn from(e: handlebars::RenderError) -> CalcverError {
+        CalcverError::with(e)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-extern crate git2;
-use git2::{Repository, Signature, Commit, ObjectType, Time, DiffOptions};
 
-mod tpl;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+#[macro_use(quick_error)]
+extern crate quick_error;
+extern crate git2;
+extern crate handlebars;
+
+
+
+use git2::{Repository, Signature, Commit, ObjectType, Time, DiffOptions};
+use handlebars::{to_json, Handlebars, Helper, JsonRender, RenderContext, RenderError};
+
 mod semver;
+mod error;
 
 pub enum VersionBumpBehavior {
     Auto,
@@ -17,13 +29,13 @@ pub struct ProjectRepo {
     pub tag_template: String
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
     message_type: String,
-    scope: String,
+    scope: Option<String>,
     short_description: String,
     description: String,
-    breaking_change: bool,
-    raw: String,
+    foot: String,
 }
 
 pub fn get_version(repo:  &ProjectRepo, bump_behavior: VersionBumpBehavior, release: bool) -> String {
@@ -41,16 +53,53 @@ pub fn get_version(repo:  &ProjectRepo, bump_behavior: VersionBumpBehavior, rele
     String::from("1.0.0")
 }
 
-pub fn format_commit_message(repo:  &ProjectRepo, msg: &Message) -> bool {
+pub fn format_commit_message(repo:  &ProjectRepo, msg: &Message) -> Result<String,error::CalcverError> {
     // convert message to commit message using template
-    true
+    let mut handlebars = Handlebars::new();
+    handlebars.register_template_string("default",repo.commit_template.to_string())?;
+    Ok(handlebars.render("default",msg)?)
 }
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    fn get_project() -> ProjectRepo {
+        ProjectRepo {
+            commit_template: String::from("{{message_type}}{{#if scope}}({{ scope }}){{/if}}: {{short_description}}\n\n{{description}}\n\n{{foot}}"),
+            path: String::from(""),
+            tag_template: String::from(""),
+        }
+    }
+    fn get_message(hasScope: bool) -> Message {
+        Message{
+            message_type: String::from("feat"),
+            scope: match hasScope {
+                true => Some(String::from("semver")),
+                _ => None
+            },
+            short_description: String::from("short"),
+            description: String::from("long"),
+            foot:String::from( "BREAKING CHANGE"),
+        }
+    }
+
     #[test]
-    fn placeholder() {
-        assert!(true,"placeholder test");
+    fn format_commit_with_scope() {
+        let repo = get_project();
+        let msg = get_message(true);
+
+        let res = format_commit_message(&repo,&msg).unwrap();
+        assert_eq!("feat(semver): short\n\nlong\n\nBREAKING CHANGE",res);
+    }
+    
+    #[test]
+    fn format_commit_without_scope() {
+        let repo = get_project();
+        let msg = get_message(false);
+
+        let res = format_commit_message(&repo,&msg).unwrap();
+        assert_eq!("feat: short\n\nlong\n\nBREAKING CHANGE",res);
     }
 }
