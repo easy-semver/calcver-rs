3825c25b3f06ee852ef3d1b08b4220fc61ebcc7d
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2018-01-07 18:04:57 +0800

    chore: updated api signature, project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-pub mod tpl;
-pub mod semver;
+extern crate git2;
+use git2::{Repository, Signature, Commit, ObjectType, Time, DiffOptions};
+
+mod tpl;
+mod semver;
 
 pub enum VersionBumpBehavior {
     Auto,
@@ -8,17 +11,39 @@ pub enum VersionBumpBehavior {
     Patch,
 }
 
-pub struct Repository {
-    path: String,
-    commit_template: String,
-    tag_template: String
+pub struct ProjectRepo {
+    pub path: String,
+    pub commit_template: String,
+    pub tag_template: String
 }
 
+pub struct Message {
+    message_type: String,
+    scope: String,
+    short_description: String,
+    description: String,
+    breaking_change: bool,
+    raw: String,
+}
 
-pub fn get_version(repo: &Repository, bump_behavior: VersionBumpBehavior, release: bool) -> String {
-    
-    
+pub fn get_version(repo:  &ProjectRepo, bump_behavior: VersionBumpBehavior, release: bool) -> String {
+    // get project commits as Message
+    // get latest reachable tag
+    // get branch name
+    // call get_next_version to determine version
+    // if release = true
+    //      generate commit log (next iteration)
+    //      update version number in files
+    //      commit changes
+    //      tag commit
+    // end if
+    // return version
     String::from("1.0.0")
+}
+
+pub fn format_commit_message(repo:  &ProjectRepo, msg: &Message) -> bool {
+    // convert message to commit message using template
+    true
 }
 
 
@@ -26,6 +51,6 @@ pub fn get_version(repo: &Repository, bump_behavior: VersionBumpBehavior, releas
 mod tests {
     #[test]
     fn placeholder() {
-        assert!(false,"placeholder test");
+        assert!(true,"placeholder test");
     }
 }
--- src/tpl.rs
+++ src/tpl.rs
@@ -1,31 +1,23 @@
+pub fn is_template_valid(template: String) -> bool {
+    true
+}
 
+pub fn is_message_valid(msg: String, tpl: String) -> bool {
+    true
+}
 
-        pub struct Message {
-            message_type: String,
-            scope: String,
-            short_description: String,
-            description: String,
-            breaking_change: bool,
-            raw: String,
-        }
+pub fn get_message(msg: String) -> super::Message {
+    let retval = super::Message {
+        message_type: String::from("tes"),
+        scope: String::from("test"),
+        short_description: String::from("test"),
+        description: String::from("test"),
+        breaking_change: false,
+        raw: String::from("")
+    };
+    retval
+}
 
-        pub fn is_template_valid(template: String) -> bool {
-            true
-        }
-
-        pub fn is_message_valid(msg: String, tpl: String) -> bool {
-            true
-        }
-
-        pub fn get_message(msg: String) -> Message {
-            let mut retval = Message {
-                message_type: String::from("tes"),
-                scope: String::from("test"),
-                short_description: String::from("test"),
-                description: String::from("test"),
-                breaking_change: false,
-                raw: String::from("")
-            };
-            retval
-        }
- 
+pub fn create_message(msg: super::Message) -> String {
+    "".to_string()
+}
